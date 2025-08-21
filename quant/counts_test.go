0d733c49374692c462b1bde27569d5ea5148f33d
Chris Okuda <chris@okuda.dev>
2025-08-21 08:35:44 -1000

    Dumping quant into the repo wholesale, but it still needs a bunch of work to move things around

---
+++ quant/counts_test.go
@@ -0,0 +1,19 @@
+package quant_test
+
+// ############################################################################
+// Tests
+// ############################################################################
+
+//TODO correctness test NewTypeCounter()
+
+//TODO correctness test (c *TypeCounter) TypeCount()
+
+//TODO correctness test (c *TypeCounter) CountTypes()
+
+// ############################################################################
+// Benchmarking
+// ############################################################################
+
+//TODO benchmark (c *TypeCounter) TypeCount()
+
+//TODO benchmark (c *TypeCounter) CountTypes()
---
+++ quant/similarity_test.go
@@ -0,0 +1,15 @@
+package quant_test
+
+// ############################################################################
+// Tests
+// ############################################################################
+
+//TODO: correctness tests for NewCosineSimilarizer()
+
+//TODO: correctness tests for (s *CosineSimilarizer) Similarity()
+
+// ############################################################################
+// Benchmarking
+// ############################################################################
+
+//TODO: benchmarking tests (s *CosineSimilarizer) Similarity()
