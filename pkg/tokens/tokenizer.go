b99d13a547e1d27b27b12c5f7e3a2b8743b81b07
Chris Okuda <chris@okuda.dev>
2025-09-01 15:21:03 -1000

    add RegexTokenizer tests and rename chunk to text

--- pkg/tokens/tokenizer.go
+++ pkg/tokens/tokenizer.go
@@ -11,7 +11,7 @@ import (
 // ############################################################################
 
 type Tokenizer interface {
-	Tokenize(chunk string) (tokens []string, err error)
+	Tokenize(text string) (tokens []string, err error)
 }
 
 // ############################################################################
@@ -73,7 +73,7 @@ func (t *RegexTokenizer) Regex() string {
 }
 
 // Tokenizes a chunk of text using [regexp.Regexp.FindAllString].
-func (t *RegexTokenizer) Tokenize(chunk string) (tokens []string, err error) {
+func (t *RegexTokenizer) Tokenize(text string) (tokens []string, err error) {
 	// Compile regexp
 	var r *regexp.Regexp
 	if r, err = regexp.Compile(t.regex); err != nil {
@@ -81,7 +81,7 @@ func (t *RegexTokenizer) Tokenize(chunk string) (tokens []string, err error) {
 	}
 
 	// Tokenize with regex
-	tokens = r.FindAllString(chunk, -1)
+	tokens = r.FindAllString(text, -1)
 
 	return tokens, nil
 }
--- pkg/tokens/tokenizer_test.go
+++ pkg/tokens/tokenizer_test.go
@@ -36,3 +36,107 @@ func TestNewRegexTokenizer(t *testing.T) {
 		require.Equal(t, regex, tok.Regex())
 	})
 }
+
+func TestRegexTokenizer(t *testing.T) {
+	testcases := []struct {
+		Name     string
+		Text     string
+		Regex    string
+		Expected []string
+	}{
+		{
+			Name:  "QuickBrownFox-REGEX_ENGLISH_WORDS",
+			Text:  "The quick brown fox jumps over the lazy fox.",
+			Regex: tokens.REGEX_ENGLISH_WORDS,
+			Expected: []string{
+				"The",
+				"quick",
+				"brown",
+				"fox",
+				"jumps",
+				"over",
+				"the",
+				"lazy",
+				"fox",
+			},
+		},
+		{
+			Name:  "QuickBrownFox-REGEX_ENGLISH_ALPHABET_ONLY",
+			Text:  "The quick brown fox jumps over the lazy fox.",
+			Regex: tokens.REGEX_ENGLISH_ALPHABET_ONLY,
+			Expected: []string{
+				"The",
+				"quick",
+				"brown",
+				"fox",
+				"jumps",
+				"over",
+				"the",
+				"lazy",
+				"fox",
+			},
+		},
+		{
+			Name:  "QuickBrownFoxWithSymbolsAndNumbers-REGEX_ENGLISH_WORDS",
+			Text:  "\tThe **&^$% quick% &brown$ ^fox@ %jumps!\n(over) [the] {lazy} 'fox'. 100% 99.9 F.B.I.\r\n _snake_case_",
+			Regex: tokens.REGEX_ENGLISH_WORDS,
+			Expected: []string{
+				"The",
+				"quick",
+				"brown",
+				"fox",
+				"jumps",
+				"over",
+				"the",
+				"lazy",
+				"fox",
+				"100",
+				// from "99.9"
+				"99",
+				"9",
+				// from "F.B.I."
+				"F",
+				"B",
+				"I",
+				// from "_snake_case_"
+				"_snake_case_",
+			},
+		},
+		{
+			Name:  "QuickBrownFoxWithSymbolsAndNumbers-REGEX_ENGLISH_ALPHABET_ONLY",
+			Text:  "\tThe **&^$% quick% &brown$ ^fox@ %jumps!\n(over) [the] {lazy} 'fox'. 100% 99.9 F.B.I.\r\n _snake_case_",
+			Regex: tokens.REGEX_ENGLISH_ALPHABET_ONLY,
+			Expected: []string{
+				"The",
+				"quick",
+				"brown",
+				"fox",
+				"jumps",
+				"over",
+				"the",
+				"lazy",
+				"fox",
+				// does not capture the numbers
+				// from "F.B.I."
+				"F",
+				"B",
+				"I",
+				// does not capture the _snake_case_
+			},
+		},
+	}
+
+	for _, tc := range testcases {
+		t.Run(tc.Name, func(t *testing.T) {
+			//setup
+			tokenizer := tokens.NewRegexTokenizer(tokens.RegexTokenizerWithRegex(tc.Regex))
+			require.NotNil(t, tokenizer)
+
+			//test
+			tokens, err := tokenizer.Tokenize(tc.Text)
+			require.NoError(t, err)
+			require.NotNil(t, tokens)
+			require.Equal(t, tc.Expected, tokens)
+		})
+	}
+}
