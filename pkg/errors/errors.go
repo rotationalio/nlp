119ac89e531ac6b421a5c077ccb781294f29b324
Chris Okuda <chris@okuda.dev>
2025-09-05 08:43:39 -1000

    sort the errors alphabetically

--- pkg/errors/errors.go
+++ pkg/errors/errors.go
@@ -3,8 +3,8 @@ package errors
 import "errors"
 
 var (
-	ErrUnequalLengthVectors = errors.New("vector arguments must have an equal number of elements")
 	ErrLanguageNotSupported = errors.New("the selected language is not supported")
 	ErrMethodNotSupported   = errors.New("the selected method is not supported")
 	ErrUndefinedValue       = errors.New("the mathematical operation has no defined value for the given arugments")
+	ErrUnequalLengthVectors = errors.New("vector arguments must have an equal number of elements")
 )
