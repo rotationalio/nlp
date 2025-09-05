c88e444c4b2726f753830ae8f7f2c2bd0c30ab58
Chris Okuda <chris@okuda.dev>
2025-09-05 07:56:08 -1000

    add some easy Vector function wrappers

--- pkg/vector/vector.go
+++ pkg/vector/vector.go
@@ -3,10 +3,22 @@ package vector
 // Vector is currently implemented by a `[]float64`.
 type Vector []float64
 
-//TODO: func (v Vector) Cosine() (cosine float64, err error) {}
+// A [Vector] wrapper for the [Cosine] function.
+func (v Vector) Cosine(other Vector) (cosine float64, err error) {
+	return Cosine(v, other)
+}
 
-//TODO: func (v Vector) DotProduct(other Vector) (product float64, err error) {}
+// A [Vector] wrapper for the [DotProduct] function.
+func (v Vector) DotProduct(other Vector) (product float64, err error) {
+	return DotProduct(v, other)
+}
 
-//TODO: func (v Vector) Magnitude() (length float64) {}
+// A [Vector] wrapper for the [Magnitude] function.
+func (v Vector) Magnitude() (length float64) {
+	return Magnitude(v)
+}
 
-//TODO: func (v Vector) Len() (elements int) {}
+// A [Vector] wrapper for the [Len] function.
+func (v Vector) Len() (elements int) {
+	return Len(v)
+}
